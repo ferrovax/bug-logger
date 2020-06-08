02909efe8e3a76db250841017be167af12daaf98
Jake Yukich <jake.yukich@gmail.com>
2020-06-08 16:45:18 -0700

    boilerplate

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './App.css';
+
+let root = document.createElement('div');
+root.id = 'root';
+document.body.appendChild(root);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
